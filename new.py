4080900ed623d157d718a57ef6f23881e8f8ef17
Eli Grosman <eligrosman1@gmail.com>
2019-12-11 15:13:18 -0800

    Work 12/10

---
+++ new.py
@@ -0,0 +1,60 @@
+import numpy as np
+import copy
+import math 
+matrix_1 = np.loadtxt('littles.csv', delimiter=',')
+matrix_2 = np.loadtxt('bigs.csv', delimiter=',')
+
+shape = (matrix_1.shape[0], matrix_2.shape[0])
+list_a = np.zeros(shape)
+for i in range(matrix_1.shape[0]):
+  for j in range(matrix_2.shape[1]):
+    list_a[j,i] = (matrix_1[j][i]*matrix_2[i][j])
+
+def cartesianProduct(a, b): 
+    result =[] 
+    for i in range(len(a)): 
+        for j in range(len(b)): 
+            if type(a[i]) != list:          
+              temp = [i+1]
+            else:
+              temp = copy.copy(a[i])
+            temp.append(j+1)  
+            doAdd = True   
+            for n in range(len(temp)):
+              if list_a[n][temp[n]-1] == 0:
+                doAdd = False
+            if(doAdd):
+              result.append(temp)   
+    return result 
+
+
+def PermutateAllSets(list_a, n): 
+    temp = list_a[0] 
+    for i in range(1, n): 
+        temp = cartesianProduct(temp, list_a[i]) 
+    result = []
+    for i in range(len(temp)):
+      sum = 0
+      for j in range(len(temp[i])):
+        sum += list_a[j][temp[i][j]-1]
+      result.append((temp[i], sum, len(set(temp[i])), math.sqrt(sum/len(set(temp[i])))))
+    return(result) 
+   
+n = len(list_a)  
+
+p = PermutateAllSets(list_a,n)
+
+energy = []
+for i in p:
+  energy.append((i[3],i))
+print(len(p))
+energy.sort()
+print(energy[0:5])
+with open('resultsnew.txt', 'w') as fp:
+    fp.write('\n'.join('%s %s' % x for x in energy))
+
+
+#print(numZeros)
+#Matches = [('L1', "B2", 3)]
+#Matches.append(('L2', 3))
+#print(Matches)
